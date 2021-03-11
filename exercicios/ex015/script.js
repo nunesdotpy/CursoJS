4d7dae5f871f70a3469b3b3d706d1e55ce1e193c
Vitor Nunes <vnunesaj@gmail.com>
2021-03-11 20:11:57 -0300

    exercicio idade

---
+++ exercicios/ex015/script.js
@@ -0,0 +1,44 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano){
+        alert('[ERRO] Verifique os dados e tente novamente')
+    }else{
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'kidm')
+            }else if (idade < 21){
+                //jovem
+                img.setAttribute('src', 'youngm')
+            }else if (idade < 65){
+                //adulto
+                img.setAttribute('src', 'adultm')
+            }else{
+                // idoso
+                img.setAttribute('src', '')
+            }
+        }else if (fsex[1].checked){
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10){
+                //criança
+            }else if (idade < 21){
+                //jovem
+            }else if (idade < 65){
+                //adulto
+            }else{
+                //idoso
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    }
+}
