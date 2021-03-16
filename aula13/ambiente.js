ef4ce7c0be354b229eff919cd392218e0430b7db
Vitor Nunes <66601995+nunesdotpy@users.noreply.github.com>
2021-03-16 20:37:16 -0300

    laços de repetição

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,1 +1,13 @@
-console.log('Tudo bem?')
+/*var c = 1
+while (c <= 10){
+    console.log(`Tudo bem ${c}`)
+    c++
+}
+*/
+
+var c = 1
+
+do{
+    console.log(`Tudo bem xD ${c}`)
+    c++
+}while (c <=10)
