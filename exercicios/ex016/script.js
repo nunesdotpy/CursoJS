4523b7ada73ae89b07ae7292f693d2c0834a53d6
Vitor Nunes <66601995+nunesdotpy@users.noreply.github.com>
2021-03-22 16:55:05 -0300

    update

--- exercicios/ex016/script.js
+++ exercicios/ex016/script.js
@@ -3,7 +3,4 @@ var fim = document.getElementById('fim')
 var passo = document.getElementById('pas')
 var mostrar = document.getElementById('show')
 
-while (inicio <= fim){
-    mostrar.innerText = `${inicio}`
-    inicio += passo
-}
+
