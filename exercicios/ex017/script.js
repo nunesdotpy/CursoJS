17dbc84a9f350cd622b27948446c86d2ad9e8dd9
Vitor Nunes <vnunesaj@gmail.com>
2021-06-26 00:54:09 -0300

    tabuada 2.0
    by gustavo guanabara

--- exercicios/ex017/script.js
+++ exercicios/ex017/script.js
@@ -2,14 +2,16 @@ function tabuada() {
     let num = document.getElementById('num')
     let res = document.getElementById('res')
     let n = Number(num.value)
-    let op
+
     res.innerHTML = `<p>Resultado: </p>`
     if (num.value.length == 0){
         alert('[ERRO] Voce deve informar um valor!')
     }else{
         for (let c = 1; c <= 10; c++){
-           op = n * c
-           res.innerHTML += `${n} x ${c} = ${op} <br>`
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            res.appendChild(item)
         }
     }
 }
